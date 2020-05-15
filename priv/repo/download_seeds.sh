57d6f52a288e1807285df18269af320f14ee7d7f
Aleksei <alexey.kashuba97@gmail.com>
2020-05-15 18:56:46 +0200

    download data from seeds.exs

--- priv/repo/download_seeds.sh
+++ priv/repo/download_seeds.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#This script is called from seeds.exs
+#Can be invoked with mix ecto.reset from the top level directory
 
-wget https://datavvizards.s3.eu-west-3.amazonaws.com/seeds.zip 
-unzip seeds.zip
+prefix="$(pwd)/priv/repo/"
+if [ ! -d $prefix"/seeds" ]; then
+    wget https://datavvizards.s3.eu-west-3.amazonaws.com/seeds.zip -P $prefix
+    unzip $prefix"/seeds.zip" -d $prefix
+fi
+    
+
