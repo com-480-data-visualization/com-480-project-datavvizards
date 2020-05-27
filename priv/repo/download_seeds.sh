7bd297820b2913374ecf80c98318339cad444f43
Aleksei <alexey.kashuba97@gmail.com>
2020-05-27 07:48:39 +0200

    add download script

---
+++ priv/repo/download_seeds.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script is called from seeds.exs
+#Can be invoked with mix ecto.reset from the top level directory
+
+prefix="$(pwd)/priv/repo/"
+if [ ! -d $prefix"/seeds" ]; then
+    wget https://datavvizards.s3.eu-west-3.amazonaws.com/seeds.zip -P $prefix
+    unzip $prefix"/seeds.zip" -d $prefix
+fi
+    
+
